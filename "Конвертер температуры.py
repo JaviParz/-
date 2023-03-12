72113bcea33f5959e5e26fbe21ce86a74c8ddcbf
JaviParz <125891126+JaviParz@users.noreply.github.com>
2023-03-12 13:09:59 +0200

    Create "Конвертер температуры.py

---
+++ "Конвертер температуры.py
@@ -0,0 +1,8 @@
+# Get temperature in Celsius from user
+celsius = float(input("Enter temperature in Celsius: "))
+
+# Convert Celsius to Fahrenheit
+fahrenheit = (celsius * 9/5) + 32
+
+# Print result
+print("Temperature in Fahrenheit:", fahrenheit)
